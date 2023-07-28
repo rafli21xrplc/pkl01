fc4a81278ed95014e78479558cf7e759662b4f55
Mochamad Surya Rafliansyah <raflisurya824@gmail.com>
2023-07-28 07:30:54 +0700

    revisi

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -26,29 +26,29 @@ class Login extends Controller
         if (isset($_POST["submit"])) {
             $username = htmlspecialchars($_POST["username"]);
             $password = htmlspecialchars($_POST["password"]);
-            $result = mysqli_query($this->conn, "SELECT * FROM users WHERE username = '$username' OR email = '$username'");
-            $row = mysqli_fetch_assoc($result);
+            $resultUsers = mysqli_query($this->conn, "SELECT * FROM users WHERE username = '$username' OR email = '$username'");
+            $row = mysqli_fetch_assoc($resultUsers);
 
             $query = "select * from mahasiswa";
             $result = mysqli_query($this->conn, $query);
-
+            
             $queryMatkul = "select * from dosen";
             $resultMatkul = mysqli_query($this->conn, $queryMatkul);
-
+            
             $queryAbsen = "select * from absen";
             $resultAbsen = mysqli_query($this->conn, $queryAbsen);
-
-            $datas['datasMahasiswa'] =   mysqli_fetch_all($result, MYSQLI_ASSOC);
-            $datas['datasDosen'] = mysqli_fetch_all($resultMatkul, MYSQLI_ASSOC);
+            
+            $datasMahasiswa =   mysqli_fetch_all($result, MYSQLI_ASSOC);
+            $datasDosen = mysqli_fetch_all($resultMatkul, MYSQLI_ASSOC);
             $datasAbsen = mysqli_fetch_all($resultAbsen, MYSQLI_ASSOC);
-
-            if (mysqli_num_rows($result) > 0) {
+            
+            if (mysqli_num_rows($resultUsers) > 0) {
                 if ($password == $row['password']) {
                     $datas = array(
                         "title" => "Homepage",
                         "login" => $username,
-                        "datasMahasiswa" => $datas['datasMahasiswa'],
-                        "datasDosen" => $datas['datasDosen'],
+                        "datasMahasiswa" => $datasMahasiswa,
+                        "datasDosen" => $datasDosen,
                         "datasAbsen" => $datasAbsen,
                         "tokens" => $row['tokens']
                     );
